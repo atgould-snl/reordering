00a2d1168e7b48596992e5d924cd169dcca589de
malphil <malphil@sandia.gov>
2024-11-18 17:09:47 -0700

    Programatically set Trilinos_Dir

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 mkdir build
 cd build
+TRILBUILD=/scratch/malphil/trilinos-devel/trilinos-builds
+TRILINOS=/scratch/malphil/trilinos-devel/Trilinos
+TRILINSTALL=$TRILBUILD/installs/ifpack2
 cmake \
+  -DTrilinos_DIR=$TRILINSTALL/lib64/cmake/Trilinos \
   -DCMAKE_CXX_STANDARD=17 \
   -DCMAKE_CXX_STANDARD_REQUIRED=ON \
   -DCMAKE_CXX_EXTENSIONS=OFF \
