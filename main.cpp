1eb93d2adb43b523b4410fe5287ea96ff7e0c5b8
malphil <malphil@sandia.gov>
2024-11-18 17:02:22 -0700

    Add simple hello world program

---
+++ main.cpp
@@ -0,0 +1,15 @@
+"Kokkos_Core.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    Kokkos::initialize(argc, argv);
+
+    {
+        Kokkos::parallel_for("HelloWorld", 1, KOKKOS_LAMBDA(const int i) {
+            printf("Hello, World from Kokkos!\n");
+        });
+    }
+    Kokkos::finalize();
+
+    return 0;
+}
